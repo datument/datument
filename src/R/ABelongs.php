c1c64014667f93f05f6776720ab957b7a24765b5
datument <datument@sina.com>
2018-02-16 19:08:10 +0800

    Add simple relations: Belongs, HasOne, HasLot

---
+++ src/R/ABelongs.php
@@ -0,0 +1,12 @@
+<?php
+
+declare( strict_types= 1 );
+
+namespace Datument\R;
+
+////////////////////////////////////////////////////////////////
+
+abstract class ABelongs extends AToOne
+{
+	#
+}
