335036f67e5c5ed7e3da998ed9e0a28d180cb616
datument <datument@sina.com>
2018-02-18 20:08:43 +0800

    add facade methods Datument::registerModel(class), Datument::getModel(string):class without code

--- src/Datument.php
+++ src/Datument.php
@@ -8,5 +8,35 @@ namespace Datument;
 
 class Datument
 {
-	#
+
+	/**
+	 * Register a model.
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @param  class $model
+	 *
+	 * @return void
+	 */
+	static public function registerModel( string$model ):viod
+	{
+		#
+	}
+
+	/**
+	 * Model name to model class.
+	 *
+	 * @static
+	 * @access public
+	 *
+	 * @param  string $model
+	 *
+	 * @return class
+	 */
+	static public function getModel( string$model ):string
+	{
+		return $model;
+	}
+
 }
